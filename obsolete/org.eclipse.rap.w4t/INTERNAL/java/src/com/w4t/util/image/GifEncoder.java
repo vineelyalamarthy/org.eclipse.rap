6242c01ec6856c8cb7d091c8fdfe774ddfe94912
fappel <fappel>
2006-11-20 12:20:32 +0000

    Integration of entities escape utility, hashcode builder and file upload library

---
+++ obsolete/org.eclipse.rap.w4t/INTERNAL/java/src/com/w4t/util/image/GifEncoder.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Note: The implementation of this class is missing, since the code is still
+ *       in approval state. The implementation will be added as soon as 
+ *       possible.
+ ******************************************************************************/
+package com.w4t.util.image;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.FileOutputStream;
+
+
+public class GifEncoder {
+
+  public GifEncoder( final BufferedImage bufferedImage, 
+                     final FileOutputStream fos )
+  {
+    throw new UnsupportedOperationException();
+  }
+
+  public GifEncoder( final BufferedImage bufferedImage, 
+                     final FileOutputStream fos, 
+                     final boolean b, 
+                     final Color transparentColor )
+  {
+    throw new UnsupportedOperationException();
+  }
+
+  public void encode() {
+    throw new UnsupportedOperationException();
+  }
+}
