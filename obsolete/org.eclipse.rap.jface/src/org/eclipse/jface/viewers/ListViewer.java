f1cc5431595bd0318232a6766f568843ed386bd6
rherrmann <rherrmann>
2007-06-25 16:43:21 +0000

    Replaced UnsupportedOperationException in ListViewer with original method calls that are meanwhile implemented in List

--- obsolete/org.eclipse.rap.jface/src/org/eclipse/jface/viewers/ListViewer.java
+++ obsolete/org.eclipse.rap.jface/src/org/eclipse/jface/viewers/ListViewer.java
@@ -234,8 +234,7 @@ public class ListViewer extends AbstractListViewer {
 	                System.arraycopy(ixs, 0, ixs = new int[count], 0, count);
 	            }
 	            list.deselectAll();
-//	            list.select(ixs);
-	            throw new UnsupportedOperationException();
+	            list.select(ixs);
 	        }
 		}
 	}
