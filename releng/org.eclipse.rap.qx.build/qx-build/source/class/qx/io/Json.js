ff97cd14a229e1fcb9c5271f7945238ade9d612d
rherrmann <rherrmann>
2007-09-26 19:59:32 +0000

    Adjusted the licence text to reflect the license that Eclipse has been permitted to use for this code, which is the Json License with the following term removed:  "The software shall be used for Good not Evil."

--- releng/org.eclipse.rap.qx.build/qx-build/source/class/qx/io/Json.js
+++ releng/org.eclipse.rap.qx.build/qx-build/source/class/qx/io/Json.js
@@ -37,8 +37,6 @@
        copies of the Software, and to permit persons to whom the Software is
        furnished to do so, subject to the following conditions:
 
-       The Software shall be used for Good, not Evil.
-
        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
