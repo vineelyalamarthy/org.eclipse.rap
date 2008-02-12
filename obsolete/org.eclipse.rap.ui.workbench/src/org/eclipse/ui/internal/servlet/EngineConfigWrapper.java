73edc9356e0869734cec336d31eff92989a766d2
rsternber <rsternber>
2008-02-12 14:50:24 +0000

    Dropped additional parameter from theme extension point

--- obsolete/org.eclipse.rap.ui.workbench/src/org/eclipse/ui/internal/servlet/EngineConfigWrapper.java
+++ obsolete/org.eclipse.rap.ui.workbench/src/org/eclipse/ui/internal/servlet/EngineConfigWrapper.java
@@ -237,16 +237,10 @@ final class EngineConfigWrapper implements IEngineConfig {
       String themeId = elements[ i ].getAttribute( "id" );
       String themeFile = elements[ i ].getAttribute( "file" );
       String themeName = elements[ i ].getAttribute( "name" );
-      String themeAppearanceExt = elements[ i ].getAttribute( "appearance_ext" );
       try {
         final Bundle bundle = Platform.getBundle( contributorName );
         URL url = bundle.getResource( themeFile );
         InputStream inStream = url.openStream();
-        InputStream inStreamAppExt = null;
-        if( themeAppearanceExt != null ) {
-          URL urlAppExt = bundle.getResource( themeAppearanceExt );
-          inStreamAppExt = urlAppExt.openStream();
-        }
         if( inStream != null ) {
           try {
             ResourceLoader resLoader = new ResourceLoader() {
@@ -264,7 +258,6 @@ final class EngineConfigWrapper implements IEngineConfig {
             ThemeManager.getInstance().registerTheme( themeId,
                                                       themeName,
                                                       inStream,
-                                                      inStreamAppExt,
                                                       resLoader );
           } finally {
             inStream.close();
