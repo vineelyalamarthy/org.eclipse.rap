b51a423d9bb2ed238007023eb7748fa8b6b8f1ac
rherrmann <rherrmann>
2009-05-04 22:33:02 +0000

    Adopted 3.5 compiler settings

--- sandbox/rms/org.eclipse.rap.rms.ui/src/org/eclipse/rap/rms/ui/internal/startup/RMSWorkbenchAdvisor.java
+++ sandbox/rms/org.eclipse.rap.rms.ui/src/org/eclipse/rap/rms/ui/internal/startup/RMSWorkbenchAdvisor.java
@@ -1,7 +1,13 @@
 // Created on 09.09.2007
 package org.eclipse.rap.rms.ui.internal.startup;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.eclipse.rap.rms.ui.Constants;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.activities.IMutableActivityManager;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
@@ -9,6 +15,15 @@ import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
 public class RMSWorkbenchAdvisor extends WorkbenchAdvisor {
 
+  public void preStartup() {
+    IWorkbench workbench = PlatformUI.getWorkbench();
+    IMutableActivityManager activitySupport
+      = workbench.getActivitySupport().createWorkingCopy();
+    Set<String> enabledActivityIds = new HashSet<String>();
+    enabledActivityIds.add( "org.eclipse.rap.rms.ui.internal.activity" );
+    activitySupport.setEnabledActivityIds( enabledActivityIds );
+  }
+  
   public String getInitialWindowPerspectiveId() {
     return Constants.PERSPECTIVE_ID;
   }
