d63f4dbbb9b9357b07c70fae57b6e62721dd6158
ifurnadjiev <ifurnadjiev>
2009-10-21 06:48:17 +0000

    Fix vertical text field alignment if border is applied.

--- releng/org.eclipse.rap.qx.build/source/class/qx/ui/form/TextField.js
+++ releng/org.eclipse.rap.qx.build/source/class/qx/ui/form/TextField.js
@@ -868,7 +868,7 @@ qx.Class.define("qx.ui.form.TextField",
 // RAP [if] Centered text field vertically
     _centerFieldVertically : function() {
       if( this._inputTag === "input" && this._inputElement ) {
-        var innerHeight = qx.html.Dimension.getAreaHeight( this.getElement() );
+        var innerHeight = qx.html.Dimension.getAreaHeight( this._getTargetNode() );
         var inputElementHeight = qx.html.Dimension.getBoxHeight( this._inputElement );
         if( inputElementHeight != 0 ) {
           if( qx.core.Variant.isSet( "qx.client", "mshtml" ) ) {
@@ -882,7 +882,7 @@ qx.Class.define("qx.ui.form.TextField",
           // [if] Set padding instead of style.position of the _inputElement.
           // style.position leads to problems with DOM events in FF 3.0.x
           // see bug 292487 and bug 284356
-          this.setPaddingTop( top );
+          this.setStyleProperty( "paddingTop", top + "px" );
         }
       }
     },
