e7e1d20bb6c21b9ab031edf15e28f2266d177e60
hstaudacher <hstaudacher>
2010-06-15 21:28:31 +0000

    Made changes to fix bug 315471: Wizard for creating WAR product configurations https://bugs.eclipse.org/bugs/show_bug.cgi?id=315471

---
+++ tooling/org.eclipse.rap.warproducts.core.test/src/org/eclipse/rap/warproducts/core/test/InfrastructureCreatorTest.java
@@ -0,0 +1,122 @@
+/******************************************************************************* 
+* Copyright (c) 2010 EclipseSource and others. All rights reserved. This
+* program and the accompanying materials are made available under the terms of
+* the Eclipse Public License v1.0 which accompanies this distribution, and is
+* available at http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*   EclipseSource - initial API and implementation
+*******************************************************************************/ 
+package org.eclipse.rap.warproducts.core.test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import junit.framework.TestCase;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.rap.warproducts.core.InfrastructreCreator;
+
+
+public class InfrastructureCreatorTest extends TestCase {
+  
+  private static final String TEMP_PROJECT = "tempProject";
+  private IFolder tempDir;
+
+  protected void setUp() throws Exception {
+    tempDir = createTempDir();
+  }
+  
+  private IFolder createTempDir() throws CoreException {
+    IWorkspace workspace = ResourcesPlugin.getWorkspace();
+    IWorkspaceRoot root = workspace.getRoot();
+    IProject project = root.getProject( TEMP_PROJECT );
+    if( !project.exists() ) {
+      project.create( null );
+      project.open( null );
+    }
+    IFolder tempFolder = project.getFolder( "temp" );
+    if( !tempFolder.exists() ) {
+      tempFolder.create( true, true, null );
+    }
+    return tempFolder;
+  }
+  
+  protected void tearDown() throws Exception {
+    deleteTempDir();
+  }
+  
+  private void deleteTempDir() throws CoreException {
+    if( tempDir != null && tempDir.exists() ) {
+      tempDir.delete( IResource.FORCE, null );
+    }
+  }
+
+  public void testRootPath() {    
+    IProject project 
+      = ResourcesPlugin.getWorkspace().getRoot().getProject( TEMP_PROJECT );
+    InfrastructreCreator icreator = new InfrastructreCreator( project );
+    IContainer root = icreator.getContainer();    
+    assertEquals( tempDir.getParent(), root );
+  }
+  
+  public void testCreateWebInf() {
+    InfrastructreCreator icreator = new InfrastructreCreator( tempDir );
+    icreator.createWebInf();    
+    IFolder webInf = tempDir.getFolder( "WEB-INF" );
+    assertTrue( webInf.exists() );
+  }  
+  
+  public void testCreateWebXml() throws IOException, CoreException {
+    InfrastructreCreator icreator = new InfrastructreCreator( tempDir);
+    icreator.createWebXml();
+    IFolder webInf = tempDir.getFolder( "WEB-INF" );
+    IFile webXml = webInf.getFile( "web.xml" );
+    assertTrue( webXml.exists() );
+    StringBuffer webxmlContent = readFile( webXml.getContents( true ) );
+    InputStream resourceStream 
+      = InfrastructreCreator.class.getResourceAsStream( "/web.xml" );
+    StringBuffer expectedContent = readFile( resourceStream );
+    assertEquals( expectedContent.toString(), webxmlContent.toString() );
+  }
+  
+  public void testCreateLaunchIni() throws IOException, CoreException {
+    InfrastructreCreator icreator = new InfrastructreCreator( tempDir );
+    icreator.createLaunchIni();
+    IFolder webInf = tempDir.getFolder( "WEB-INF" );
+    IFolder eclipseFolder = webInf.getFolder( "eclipse" );
+    assertTrue( eclipseFolder.exists() );
+    IFile launchIni = eclipseFolder.getFile( "launch.ini" );
+    assertTrue( launchIni.exists() );
+    StringBuffer actualLaunchIni = readFile( launchIni.getContents() );
+    InputStream tempLaunchIni 
+      = InfrastructreCreator.class.getResourceAsStream( "/launch.ini" );
+    StringBuffer expectedLaunchIni = readFile( tempLaunchIni );
+    assertEquals( expectedLaunchIni.toString(), actualLaunchIni.toString() );
+  }
+
+  private StringBuffer readFile( final InputStream fileStream ) 
+    throws IOException
+  {
+    InputStreamReader streamReader = new InputStreamReader( fileStream );
+    BufferedReader reader = new BufferedReader( streamReader );
+    StringBuffer webxmlContent = new StringBuffer();
+    int c;
+    while( ( c = reader.read() ) != -1 ) {
+      webxmlContent.append( ( char ) c );
+    }
+    reader.close();
+    return webxmlContent;
+  }
+
+}
---
+++ tooling/org.eclipse.rap.warproducts.core/src/org/eclipse/rap/warproducts/core/InfrastructreCreator.java
@@ -0,0 +1,116 @@
+/******************************************************************************* 
+* Copyright (c) 2010 EclipseSource and others. All rights reserved. This
+* program and the accompanying materials are made available under the terms of
+* the Eclipse Public License v1.0 which accompanies this distribution, and is
+* available at http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*   EclipseSource - initial API and implementation
+*******************************************************************************/ 
+package org.eclipse.rap.warproducts.core;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+
+
+public class InfrastructreCreator {
+
+  private static final String ECLIPSE_PATH = "eclipse";
+  private static final String WEB_INF_PATH = "WEB-INF";
+  private static final String WEB_XML_TEMPLATE_PATH = "/web.xml";
+  private static final String LAUNCH_INI_TEMPLATE_PATH = "/launch.ini";
+  private static final String WEB_XML_NAME = "web.xml";
+  private static final String LAUNCH_INI_NAME = "launch.ini";
+  private IContainer rootFolder;
+  private IFolder webInfDir;
+
+  public InfrastructreCreator( final IContainer tempDir ) {
+    this.rootFolder = tempDir;
+  }
+
+  public IContainer getContainer() {
+    return rootFolder;
+  }
+
+  public void createWebInf() {
+    if( webInfDir == null ) {
+      IPath webInfPath = new Path( WEB_INF_PATH );
+      webInfDir = rootFolder.getFolder( webInfPath );
+      if( !webInfDir.exists() ) {
+    	  try {
+          webInfDir.create( true, false, null );
+        } catch( final CoreException e ) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+
+  public void createWebXml() {
+    if( webInfDir == null ) {
+      createWebInf();
+    }
+    internalCopyFile( WEB_XML_TEMPLATE_PATH, webInfDir, WEB_XML_NAME );
+  }
+  
+  public void createLaunchIni() {
+    if( webInfDir == null ) {
+    	createWebInf();
+    }
+    IFolder eclipseDir = createEclipseDir();
+    internalCopyFile( LAUNCH_INI_TEMPLATE_PATH, eclipseDir, LAUNCH_INI_NAME );
+  }
+
+  private IFolder createEclipseDir() {
+    IFolder eclipseDir = webInfDir.getFolder( ECLIPSE_PATH );
+    if( !eclipseDir.exists() ) {
+    	try {
+        eclipseDir.create( true, true, null );
+      } catch( final CoreException e ) {
+        System.err.println( "Could not create eclipse directory" );
+        e.printStackTrace();
+      }
+    }
+    return eclipseDir;
+  }
+  
+  private void internalCopyFile( final String from, 
+                                 final IContainer container,
+                                 final String fileName ) 
+  {
+    try {
+      copyFile( from, container, fileName );
+    } catch( final IOException e ) {
+      e.printStackTrace();
+    }
+  }
+
+  private void copyFile( final String from, 
+                         final IContainer container, 
+                         final String fileName ) throws IOException 
+  {	  	  
+      IPath path = new Path( fileName );
+      IFile file = container.getFile( path );
+      if( !file.exists() ) {
+        try {
+          file.create( getFileStream( from ), true, null );
+        } catch( final CoreException e ) {
+          System.err.println("Could not copy file: " + from );
+          e.printStackTrace();
+        }
+      }
+  }
+
+  private InputStream getFileStream( final String from ) throws IOException {
+    InputStream fileInStream = getClass().getResourceAsStream( from );
+    return fileInStream;
+  }
+
+}
